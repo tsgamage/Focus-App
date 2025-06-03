353068c9d138023923f3a5c9cd013da30459b39c
tsgamage <thevidu2007627@gmail.com>
2025-06-04 00:40:13 +0530

    Feat: Make the app timer working and change the timer time according to the loop time

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 import tkinter as tk
 from tkinter import messagebox
+import math
 
 
 class FocusApp(tb.Window):
@@ -9,6 +10,16 @@ class FocusApp(tb.Window):
         super().__init__(themename='superhero')
 
         app_icon = tk.PhotoImage(file="./assets/icon/Focus.png")
+        self.loop_time = 0
+
+        self.WORK_MIN = 5
+        self.SHORT_BREAK_MIN = 3
+        self.LONG_BREAK_MIN = 7
+
+        # Timing in seconds
+        self.WORKING_TIME_SEC = self.WORK_MIN
+        self.SMALL_BREAK_SEC = self.SHORT_BREAK_MIN
+        self.LONG_BREAK_SEC = self.LONG_BREAK_MIN
 
         self.title("Focus App")
         self.geometry("350x500")
@@ -46,7 +57,7 @@ class FocusApp(tb.Window):
             amounttotal=60,
             amountused=0,
             metertype="semi",
-            subtext="miles per hour",
+            # subtext="miles per hour",
             meterthickness=20,
             stripethickness=3,
             showtext=False,
@@ -54,8 +65,8 @@ class FocusApp(tb.Window):
         )
         self.meter.grid(row=2, column=0, columnspan=2)
 
-        self.over_thing = tb.Label(master=self.main_tab, text="25:00", font=("poppins", 22), bootstyle="primary")
-        self.over_thing.grid(row=2, column=0, columnspan=2)
+        self.timer = tb.Label(master=self.main_tab, text="25:00", font=("poppins", 22), bootstyle="primary")
+        self.timer.grid(row=2, column=0, columnspan=2)
 
         self.sound_tick_value = tb.StringVar()
         self.sound_tick_value.set("1")
@@ -150,3 +161,68 @@ class FocusApp(tb.Window):
         tb.Combobox(settings_win, values=["Superhero", "Darkly", "Flatly"]).pack()
 
         tb.Button(settings_win, text="Save", command=settings_win.destroy).pack(pady=20)
+
+    # The function that's control the app's state
+    def call_timer(self):
+        """
+        The interval periods of this app
+
+        1: 25 minutes of work,
+        2: 5-minute break,
+        3: 25 minutes of work,
+        4: 5-minute break,
+        5: 25 minutes of work,
+        6: 5-minute break,
+        7: 25 minutes of work,
+        8: 20 minutes of long break
+
+        """
+
+        #  Keep track of how many times this function called
+        self.loop_time += 1
+        # Removed always on top status from app, So user can use another app without distracted from with this app's window
+        self.attributes("-topmost", False)
+
+        # If this function runs 8 times, It means it's time for a long break
+        if self.loop_time == 8:
+            self.update_timer(self.LONG_BREAK_SEC)
+
+        #  All the loop time numbers for a short break are odd value, So this will check whether its odd and set app's state to short Break
+        elif self.loop_time % 2 == 0:
+            self.update_timer(self.SMALL_BREAK_SEC)
+
+        #  All the loop time numbers for a Working time are even value, So this will check whether its even and set app's state to working
+        elif self.loop_time % 2 == 1:
+            self.update_timer(self.WORKING_TIME_SEC)
+
+    def update_timer(self, count):
+        timer_min = math.floor(count / 60)
+        timer_sec = count % 60
+
+        # Adding a '0' to the front of minutes when it is smaller than 10
+        if timer_min < 10:
+            timer_min = f"0{timer_min}"
+
+        # Adding a '0' to the front of seconds when it is smaller than 10
+        if timer_sec < 10:
+            timer_sec = f"0{timer_sec}"
+
+        # Adding 00 to the seconds when it is zero
+        if timer_sec == 0:
+            timer_sec = "00"
+
+        self.timer.configure(text=f"{timer_min}:{timer_sec}")
+
+        if count > 0:
+            global timer
+            # Runs the whole function continuously after every second
+            timer = self.after(1000, self.update_timer, count - 1)
+
+        else:
+            # When the timer hits 'zero,' it means it's time to change the app state (Working, Short Break, Long Break)
+            # Before calling the call_timer function, the app needs to go to the top of the screen
+            # So, the user knows when it's time to a break, or it's time to work
+            self.attributes("-topmost", True)
+
+            # After the application comes to the top of the screen, the call_timer function will be executed
+            self.call_timer()
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from gui import FocusApp
 
 if __name__ == "__main__":
     app = FocusApp()
+    app.call_timer()
     app.mainloop()
