5524d06947499b0ae3920418465058ebb2ac4885
tsgamage <thevidu2007627@gmail.com>
2025-06-10 00:51:06 +0530

    fix: application crashes when request time out
    add exceptions to the update request and handle the app crash when user not connected to the internet

--- focus/autoUpdate.py
+++ focus/autoUpdate.py
@@ -12,14 +12,13 @@ class AutoUpdate(UpdateWindow):
     def __init__(self):
         super().__init__()
 
-        # self.api_url = "https://api.github.com/repos/tsgamage/Focus-App/releases/latest"
-        self.api_url = "http://localhost:3000/"
+        self.api_url = "https://api.github.com/repos/tsgamage/Focus-App/releases/latest"
+        # self.api_url = "http://localhost:3000/"
 
         self.save_path = "C:\\Users\\Public\\Downloads\\FocusApp"
         os.makedirs(self.save_path, exist_ok=True)
         self.current_version = "1.0.0"
-        self.get_current_app_info()
-        self.update_info = {}
+        self.update_info = {"update_available": False}
         self.installer_path = None
 
 
@@ -29,22 +28,34 @@ class AutoUpdate(UpdateWindow):
         self.current_version = app_info["version"]
 
     def check_for_update(self, current_version):
-        response = requests.get(self.api_url, timeout=5)
+        try:
+            response = requests.get(self.api_url, timeout=5)
+        except:
+            print(f"Error: Could not connect to GitHub API.")
+        else:
+            print(f"Connected to GitHub API.")
+            if response.status_code == 200:
+                latest = response.json()
+                latest_version = latest["tag_name"].lstrip("v")
 
-        if response.status_code == 200:
-            latest = response.json()
-            latest_version = latest["tag_name"].lstrip("v")
+                if latest_version != current_version:
+                    self.update_info = {
+                        "update_available": True,
+                        "name": latest["name"],
+                        "version": latest_version,
+                        "release_notes": latest.get("body", "No details."),
+                        "size": latest["assets"][0]["size"],
+                        "url": latest["assets"][0]["browser_download_url"]
+                    }
 
-            if latest_version != current_version:
-                return {
-                    "update_available": True,
-                    "name": latest["name"],
-                    "version": latest_version,
-                    "release_notes": latest.get("body", "No details."),
-                    "size": latest["assets"][0]["size"],
-                    "url": latest["assets"][0]["browser_download_url"]
-                }
-        return {"update_available": False}
+        if self.update_info["update_available"]:
+            print("Update available.")
+            do_update = messagebox.askyesno("Update Available",
+                                            f"A new version v{self.update_info['version']} is available!\nDo you want to download and install it?")
+
+            if do_update:
+                self.create_downloading_window()
+
 
     def download_update(self,version, url,file_size):
         save_path = self.save_path + f"\\FocusAppSetup_{version}.exe"
@@ -64,13 +75,7 @@ class AutoUpdate(UpdateWindow):
         threading.Thread(target=self.download_update, args=(self.update_info["version"], self.update_info["url"],self.update_info["size"])).start()
 
     def update_app(self):
-        self.update_info = self.check_for_update(self.current_version)
-        if self.update_info["update_available"]:
-            do_update = messagebox.askyesno("Update Available",
-                                            f"A new version v{self.update_info['version']} is available!\nDo you want to download and install it?")
-
-            if do_update:
-                self.create_downloading_window()
+        threading.Thread(target=self.check_for_update, args=(self.current_version,)).start()
 
     def finish_downloading(self):
         self.window.destroy()
