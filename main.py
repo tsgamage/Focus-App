a0af2147911c6ccd1671faeb11d7da6fc6307c4b
tsgamage <thevidu2007627@gmail.com>
2025-08-17 17:58:08 +0530

    fix: App doesn't launch in first run. Changed the is_app_running logic
    Previously, the method checked for a single match of the app name in the task list. This update ensures the app is only detected if it appears more than twice. It improves reliability in cases of duplicate or unrelated matches.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from tkinter.messagebox import showinfo
 
 def is_app_running(app_name):
     tasks = os.popen('tasklist').read().lower()
-    return app_name.lower() in tasks
+    # return app_name.lower() in tasks
+    return tasks.count(app_name.lower()) > 2
 
 if __name__ == "__main__":
     if is_app_running("FocusApp.exe"):
